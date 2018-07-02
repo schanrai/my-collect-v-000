d83714e2ab0cea535ad6891ecc8a2880d71d08c1
Sushi Chanrai <sushi@pathworks.co>
2018-07-02 19:07:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,11 @@
-def my_collect(array)
-  
+def my_collect(languages)
+  i = 0
+  collection = []
+  while i < languages.length
+  collection << yield languages[i]
+  i = i+1
+end
+collection
 end
 
 
